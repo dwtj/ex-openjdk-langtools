1e5df4900147e1a138b9035ce278f07548799880
David Johnston <dwtj@iastate.edu>
2016-06-01 21:58:47 -0500

    Make `TypesProc` cast compilation units to JCTree

--- proc/src/main/java/me/dwtj/ex/openjdk/langtools/types/TypesProc.java
+++ proc/src/main/java/me/dwtj/ex/openjdk/langtools/types/TypesProc.java
@@ -1,11 +1,18 @@
 package me.dwtj.ex.openjdk.langtools.types;
 
+import com.sun.source.util.Trees;
+import com.sun.tools.javac.tree.JCTree.JCCompilationUnit;
+
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -16,9 +23,24 @@ import java.util.Set;
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 public class TypesProc extends AbstractProcessor {
 
+    public void init(ProcessingEnvironment procEnv) {
+        super.init(procEnv);
+    }
+
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        System.out.println("Hello, World!");
+        for (JCCompilationUnit cu : getCompilationUnits(roundEnv)) {
+            System.out.println(cu.toString());
+        }
         return false;
+    }
+
+    private List<JCCompilationUnit> getCompilationUnits(RoundEnvironment roundEnv) {
+        Trees treeUtils = Trees.instance(processingEnv);
+        List<JCCompilationUnit> list = new ArrayList<>();
+        for (Element root : roundEnv.getRootElements()) {
+            list.add((JCCompilationUnit) treeUtils.getPath(root).getCompilationUnit());
+        }
+        return list;
     }
 }
